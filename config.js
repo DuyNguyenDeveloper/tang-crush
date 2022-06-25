ffddf7c46503ac960f4ed124feff1459b765b28a
DuyNguyenDeveloper <77428237+DuyNguyenDeveloper@users.noreply.github.com>
2022-06-26 02:59:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Anh thương công chúa của anh nhiềuuuuuuuuu <3333.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+//     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
